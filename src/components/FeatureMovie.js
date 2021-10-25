8d61d8a3cce800750628c7e68bd5eb7d770795d5
BeatrizMachado <beatriz.machadosilva@gmail.com>
2021-10-25 20:38:30 -0300

    update

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -3,6 +3,14 @@ import './FeatureMovie.css';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({item})=> {
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
     return (
        <section className="featured" style={{
            backgroundSize: 'cover',
@@ -11,17 +19,18 @@ export default ({item})=> {
        }}>
             <div className="featured--vertical">
                 <div className="featured--horizontal">
-                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--name">{item.name}</div>
                         <div className="featured--info">
                             <div className="featured--points">{item.vote_average} pontos</div>
-                            <div className="featured--year">2099</div>
+                            <div className="featured--year">{firstDate.getFullYear()}</div>
                             <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                         </div>
                         <div className="featured--description">{item.overview}</div>
                         <div className="featured--buttons">
-
+                            <a className="featured--watchButton" href={`/watch/${item.id}`}>► Assistir</a>
+                            <a className="featured--listButton" href={`/list/add/${item.id}`}>+ Minha Lista</a>
                         </div>
-                        <div className="featured--genres">Gêneros: </div>
+                        <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
                 </div>
             </div>
        </section>
